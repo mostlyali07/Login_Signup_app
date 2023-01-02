fb4957ce1c4be74e464fb86fdd3e1c85df8fa46c
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2023-01-02 17:30:25 +0500

    Layout done

--- App.js
+++ App.js
@@ -16,16 +16,15 @@ import {
   Text,
   useColorScheme,
   View,
+  text,
+  password,
+  Button,
+  onChangeNumber,
 } from 'react-native';
 
-import {
-  Colors,
-  number,
-  onChangeNumber,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-
-const App: () => Node = () => {
+const LogINNode = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -49,16 +48,27 @@ const App: () => Node = () => {
         </View>
         <View
           style={{
-            backgroundColor: 'white',
+            backgroundColor: 'white', marginTop: '10',
           }}>
           <Text style={[styles.highlightTwo]}>Login</Text>
           <TextInput
             style={styles.input}
             onChangeText={onChangeNumber}
-            value={number}
-            placeholder="useless placeholder"
-            keyboardType="numeric"
+            value={text}
+            placeholder="Enter Email"
+            keyboardType="text"
           />
+          <TextInput
+            style={styles.inputTwo}
+            value={password}
+            placeholder="Enter Password"
+            keyboardType="password"
+            secureTextEntry={true}
+          />
+          <View style={styles.ButtonOne}>
+            <Button title="login" color="blue" borderRadius="50" />
+          </View>
+          <Text  style={styles.TextBottom}>Create an account? Sign Up</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -76,9 +86,37 @@ const styles = StyleSheet.create({
   highlightTwo: {
     fontSize: 30,
     margin: 50,
+    marginBottom: 10,
     fontWeight: '600',
     color: 'black',
   },
+  input: {
+    marginBottom: 10,
+    marginTop: 10,
+    height: 40,
+    margin: 50,
+    borderWidth: 1,
+    padding: 10,
+  },
+  inputTwo: {
+    marginTop: 10,
+    marginBottom: 10,
+    height: 40,
+    margin: 50,
+    borderWidth: 1,
+    padding: 10,
+  },
+  ButtonOne: {
+    marginTop: 5,
+    marginBottom: 10,
+    marginLeft: 120,
+    marginRight: 120,
+  },
+  TextBottom:{
+    fontWeight: '600',
+    color: 'black',
+    textAlign: 'center',
+  },
 });
 
-export default App;
+export default LogINNode;
